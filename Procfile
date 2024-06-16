65a9c7327421d6a101758b85a89f15c7ea00ef67
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-15 22:44:18 -0400

    Boilerplate

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: node --optimize_for_size --max_old_space_size=460 --gc_interval=100 index.js
+web: node --optimize_for_size --max_old_space_size=460 --gc_interval=100 build/server.js
+release: node build/ace migration:run --force
---
+++ config/ally.ts
@@ -0,0 +1,27 @@
+import env from '#start/env'
+import { defineConfig, services } from '@adonisjs/ally'
+
+defineConfig({
+  github: services.github({
+    clientId: env.get('GITHUB_CLIENT_ID')!,
+    clientSecret: env.get('GITHUB_CLIENT_SECRET')!,
+    callbackUrl: `${env.get('HOST')}/auth/callback/github`,
+  }),
+  google: services.google({
+    clientId: env.get('TWITTER_CLIENT_ID')!,
+    clientSecret: env.get('TWITTER_CLIENT_SECRET')!,
+    callbackUrl: `${env.get('HOST')}/auth/callback/google`,
+
+    // Google specific
+    prompt: 'select_account',
+    accessType: 'offline',
+    hostedDomain: 'nvp.gg',
+    display: 'page',
+    scopes: ['userinfo.email'],
+  }),
+  twitter: services.twitter({
+    clientId: env.get('TWITTER_CLIENT_ID')!,
+    clientSecret: env.get('TWITTER_CLIENT_SECRET')!,
+    callbackUrl: `${env.get('HOST')}/auth/callback/x`,
+  }),
+})
