779a4b884b883772f3447c779152343fd491b2ee
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-17 09:18:39 -0400

    Start to fix oauth

--- api/app/controllers/home.ts
+++ api/app/controllers/home.ts
@@ -1,12 +1,15 @@
 import type { HttpContext } from '@adonisjs/core/http';
+import env from '#start/env';
 
 export default class LinksController {
   async index({ request, view }: HttpContext) {
     let host = request.host();
     let isLocal = host?.includes('localhost');
+    let isDev = env.get('NODE_ENV') === 'development';
 
     return view.render('index', {
       isLocal,
+      isDev,
     });
   }
 }
