461d99c07c4582e09e59e57531376852145b9ab8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-16 20:23:27 -0400

    lint:fix

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -5,5 +5,8 @@ module.exports = {
   rules: {
     // I love shadows
     '@typescript-eslint/no-shadow': 'off',
+    // filename conventions have no place if if we're going
+    // to wire everything up with explicit imports
+    'unicorn/filename-case': 'off',
   },
 }
