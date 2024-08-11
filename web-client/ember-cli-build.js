682b86191619d01d7c6792142a56b6705888225d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-11 19:20:28 -0400

    Vite + ember-data still don't currently work

--- web-client/ember-cli-build.js
+++ web-client/ember-cli-build.js
@@ -3,8 +3,14 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { maybeEmbroider } = require('@embroider/test-setup');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
+  const { setConfig } = await import('@warp-drive/build-config');
+
   let app = new EmberApp(defaults, {});
+
+  setConfig(app, __dirname, {
+    // WarpDrive/EmberData settings go here (if any)
+  });
 
   return maybeEmbroider(app, {
     skipBabel: [
@@ -12,5 +18,13 @@ module.exports = function (defaults) {
         package: 'qunit',
       },
     ],
+    amdCompatibility: {
+      es: [
+        /**
+         * ember-data
+         */
+        ['ember-data/store', ['default']],
+      ],
+    },
   });
 };
