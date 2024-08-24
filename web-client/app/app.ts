92cc6fec641e1df1308c9247368e92e29827ab8b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-24 17:08:21 -0400

    lint:fix

--- web-client/app/app.ts
+++ web-client/app/app.ts
@@ -1,10 +1,14 @@
 import Application from '@ember/application';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import compatModules from '@embroider/core/entrypoint';
 
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 import config from 'web-client/config/environment';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 let d = window.define;
 
 for (const [name, module] of Object.entries(compatModules)) {
