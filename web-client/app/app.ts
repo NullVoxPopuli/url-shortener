dcc3a097a815f50a87876deac78fd29c5da791cd
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-28 23:36:49 -0400

    Updates

--- web-client/app/app.ts
+++ web-client/app/app.ts
@@ -1,26 +1,15 @@
 import Application from '@ember/application';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import compatModules from '@embroider/core/entrypoint';
+import compatModules from '@embroider/virtual/compat-modules';
 
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
-import config from 'web-client/config/environment';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-let d = window.define;
-
-for (const [name, module] of Object.entries(compatModules)) {
-  d(name, function () {
-    return module;
-  });
-}
+import config from './config/environment';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
