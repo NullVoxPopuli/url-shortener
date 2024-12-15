927b9220747f79cbdeac61284cb627fdc18327fd
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-12-15 13:25:07 -0500

    lints

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,2 +1,16 @@
 import { configApp } from '@adonisjs/eslint-config';
-export default configApp();
+const base = configApp();
+
+export default [
+  ...base,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      // We like shadows here
+      '@typescript-eslint/no-shadow': 'off',
+
+      // Filename conventions are silly
+      '@unicorn/filename-case': 'off',
+    },
+  },
+];
