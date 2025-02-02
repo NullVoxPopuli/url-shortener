985c86b661112393a6a86f64242b05497d32a98d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-02 17:47:21 -0500

    Upgrade to ts-node-maintained

--- api/ace.js
+++ api/ace.js
@@ -19,8 +19,8 @@
 /**
  * Register hook to process TypeScript files using ts-node
  */
-import { register } from 'node:module';
-register('ts-node/esm', import.meta.url);
+
+import 'ts-node-maintained/register/esm';
 
 /**
  * Import ace console entrypoint
