9c2b78bf62405d78a3b984cfe3025f6fd89f1d0e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-09-21 01:38:07 -0400

    ope

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -34,7 +34,6 @@ router
       // document) and only emits links whose named routes exist.
       router.get('links', [links, 'index']).as('link.index');
       router.post('links', [links, 'create']).as('link.store');
-      // Links are not updatable (for now?)
       router.get('links/:id', [links, 'show']).as('link.show');
       router.patch('links/:id', [links, 'update']).as('link.update');
       router.delete('links/:id', [links, 'delete']).as('link.destroy');
